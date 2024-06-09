428c026924d07a3e49f1d1699df60d7af2a16146
xytrux <helloworldpy103@gmail.com>
2024-06-09 16:47:03 -0700

    feat: Extract pinned repositories from HTML and build metadata
    This commit adds functionality to extract pinned repositories from HTML and build metadata for each repository. It introduces the `nodeOfClass` procedure to filter XML nodes based on their class attribute. The `buildMetaPinnedRepo` procedure is used to populate a table with repository information.

--- main.nim
+++ main.nim
@@ -1,20 +1,13 @@
-import std/httpclient
-import strformat
-import std/htmlparser
-import std/[xmltree, strutils]
-import std/tables
-
-var user = "xytrux"
-
-var client = newHttpClient()
+import std/[xmltree, strutils, htmlparser, tables]
 
 iterator extractWithTag(x: XmlNode, name:string): XmlNode {.closure.}=
-  if(x.len > 0):
-    for i in 0..<x.len:
-      for sub in extractWithTag(x[i], name):
-        yield sub
-  if x.kind() == xnElement and x.attr(name) != "":
-    yield x
+  if x.kind == xnElement:
+    if(x.len > 0):
+      for i in 0..<x.len:
+        for sub in extractWithTag(x[i], name):
+          yield sub
+    if x.attr(name) != "":
+      yield x
 
 proc containsClass(classValue: string, lookFor: string): bool=
   for x in classValue.split(" "):
@@ -22,9 +15,21 @@ proc containsClass(classValue: string, lookFor: string): bool=
       return true
   return false
 
-# TODO: collect everything into a map.
+proc nodeOfClass(n: XmlNode, class: string): seq[XmlNode]=
+  result = newSeq[XmlNode]()
+  for ele in n.extractWithTag("class"):
+    if ele.attr("class").containsClass(class):
+      result.add ele
 
-let g = parseHtml(client.getContent(fmt"http://github.com/{user}"))
-for ele in g.extractWithTag("class"):
-  if ele.attr("class").containsClass("pinned-item-list-item"):
-    echo "found a pinned repo"
+
+proc buildMetaPinnedRepo(n: XmlNode): TableRef[string, string]=
+  result = newTable[string, string]()
+  # use the information in `n` to populate the table
+  result["full_name"] = "implement"
+  result["name"] = "implement"
+
+var repoInfos = newSeq[TableRef[string, string]]()
+
+for contentEle in g.nodeOfClass("pinned-item-list-item-content"):
+  for descEle in contentEle.nodeOfClass("pinned-item-desc"):
+    repoInfos.add(buildMetaPinnedRepo(descEle))
