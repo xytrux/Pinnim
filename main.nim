de51e83df6fe7ab23db02d5bd9ea8b32a30b3e86
xytrux <helloworldpy103@gmail.com>
2024-06-09 14:50:15 -0700

    chore: Add HTML parsing functionality and extract pinned repositories

--- main.nim
+++ main.nim
@@ -1,13 +1,30 @@
 import std/httpclient
 import strformat
 import std/htmlparser
-
-# TODO: Add a way to parse HTML
+import std/[xmltree, strutils]
+import std/tables
 
 var user = "xytrux"
 
 var client = newHttpClient()
-try:
-  echo client.getContent(fmt"http://github.com/{user}")
-finally:
-  client.close()
+
+iterator extractWithTag(x: XmlNode, name:string): XmlNode {.closure.}=
+  if(x.len > 0):
+    for i in 0..<x.len:
+      for sub in extractWithTag(x[i], name):
+        yield sub
+  if x.kind() == xnElement and x.attr(name) != "":
+    yield x
+
+proc containsClass(classValue: string, lookFor: string): bool=
+  for x in classValue.split(" "):
+    if(x == lookFor):
+      return true
+  return false
+
+# TODO: collect everything into a map.
+
+let g = parseHtml(client.getContent(fmt"http://github.com/{user}"))
+for ele in g.extractWithTag("class"):
+  if ele.attr("class").containsClass("pinned-item-list-item"):
+    echo "found a pinned repo"
