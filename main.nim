d605fd072821e56dde62c39b182a53c9571c8757
xytrux <helloworldpy103@gmail.com>
2024-06-09 20:18:29 -0700

    finally made it work! now time to put into a web server

--- main.nim
+++ main.nim
@@ -1,8 +1,4 @@
-import std/httpclient
-import strformat
-import std/htmlparser
-import std/[xmltree, strutils]
-import std/tables
+import std/[httpclient, strformat, htmlparser, xmltree, strutils, tables]
 
 var user = "xytrux"
 var client = newHttpClient()
@@ -31,25 +27,32 @@ proc nodeOfClass(n: XmlNode, class: string): seq[XmlNode]=
 
 let g = parseHtml(client.getContent(fmt"http://github.com/{user}"))
 
+
 proc buildMetaPinnedRepo(n: XmlNode): TableRef[string, string]=
   result = newTable[string, string]()
   # use the information in `n` to populate the table
+
   let full_name = n.nodeOfClass("Link")[0].attr("href")[1..^1]
+  let repo = full_name.split("/")[1]
+  let description = n.nodeOfClass("pinned-item-desc")[0].innerText.strip()
+  let repo_meta = n.nodeOfClass("pinned-item-meta")
+  let language = n.nodeOfClass("d-inline-block")[0].innerText.strip()
+  let language_color = n.nodeOfClass("repo-language-color")[0].attr("style").split(":")[1].strip()
+
   result["full_name"] = full_name
-  result["name"] = "TODO"
-  result["description"] = "TODO"
-  result["link"] = fmt"https://{full_name}"
-  result["stars"] = "TODO"
-  result["forks"] = "TODO"
-  result["language_color"] = "TODO"
-  result["language"] = "TODO"
+  result["repo"] = repo.join("")
+  result["description"] = description
+  result["link"] = fmt"https://github.com/{full_name}"
+  result["stars"] = repo_meta[0].innerText.strip()
+  if len(repo_meta) < 2:
+    result["forks"] = "0"
+  result["language_color"] = language_color
+  result["language"] = language
   return result
 
 var repoInfos = newSeq[TableRef[string, string]]()
 
 for contentEle in g.nodeOfClass("pinned-item-list-item-content"):
   repoInfos.add(buildMetaPinnedRepo(contentEle))
-
-for ele in g.extractWithTag("class"):
-  if ele.attr("class").containsClass("pinned-item-list-item"):
-    echo repoInfos
+  
+echo repoInfos
