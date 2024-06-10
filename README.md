95c38fb4cdfcbd11659d083f6781e7ec40732a61
xytrux <helloworldpy103@gmail.com>
2024-06-09 17:13:38 -0700

    add back httpclient functionality + README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pinnim
+pinned repos api made in nim
+
+## Credits
+Credit to @graveflo on Discord for helping me on this project
--- main.nim
+++ main.nim
@@ -1,4 +1,12 @@
-import std/[xmltree, strutils, htmlparser, tables]
+import std/httpclient
+import strformat
+import std/htmlparser
+import std/[xmltree, strutils]
+import std/tables
+
+var user = "xytrux"
+
+var client = newHttpClient()
 
 iterator extractWithTag(x: XmlNode, name:string): XmlNode {.closure.}=
   if x.kind == xnElement:
@@ -25,10 +33,21 @@ proc nodeOfClass(n: XmlNode, class: string): seq[XmlNode]=
 proc buildMetaPinnedRepo(n: XmlNode): TableRef[string, string]=
   result = newTable[string, string]()
   # use the information in `n` to populate the table
-  result["full_name"] = "implement"
-  result["name"] = "implement"
+  result["full_name"] = "TODO"
+  result["name"] = "TODO"
+  result["description"] = "TODO"
+  result["link"] = "TODO"
+  result["stars"] = "TODO"
+  result["forks"] = "TODO"
+  result["language_color"] = "TODO"
+  result["language"] = "TODO"
 
 var repoInfos = newSeq[TableRef[string, string]]()
+
+let g = parseHtml(client.getContent(fmt"http://github.com/{user}"))
+for ele in g.extractWithTag("class"):
+  if ele.attr("class").containsClass("pinned-item-list-item"):
+    echo "found a pinned repo"
 
 for contentEle in g.nodeOfClass("pinned-item-list-item-content"):
   for descEle in contentEle.nodeOfClass("pinned-item-desc"):
